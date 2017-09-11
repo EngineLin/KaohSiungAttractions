77b304c020b155bbf4e21175ba1cd7ca511a747d
EngineLin <linengine@gmail.com>
2017-09-11 08:05:22 +0800

    add data.json

--- all.js
+++ all.js
@@ -7,7 +7,7 @@ var filerData = [];
 
 // 高雄旅遊景點JSON
 var xhr = new XMLHttpRequest();
-xhr.open('get','http://data.kcg.gov.tw/api/action/datastore_search?resource_id=92290ee5-6e61-456f-80c0-249eae2fcc97',true);
+xhr.open('get','data.json',true);
 xhr.send(null);
 
 // 等待JSON載入後設定下方程序
